a280db63953deb5ca9724c671d49f8e61d707c1e
Zachkery Rodriguez <39169752+ZVR999@users.noreply.github.com>
2018-07-03 10:54:57 -0500

    Added an html to display the js files in console. Practiced algos

---
+++ algorithm_1.js
@@ -0,0 +1,47 @@
+var x = [1,2,3,4,5,10]
+for(var i=0; i<5;i++){   
+   console.log(i);
+}
+
+var x = [1,2,3,4,5,10]
+for(var i=0; i<5; i++)
+{
+   i = i + 1;  
+   console.log(i);
+}
+var x = [1,2,3,4,5,10]
+for(var i=0; i<5; i++)
+{
+   i = i + 3; 
+   console.log(i);
+}
+function y(num1, num2){   
+    return num1+num2;
+ }
+ console.log(y(2,3))
+ console.log(y(3,5))
+
+ function z(num1, num2){
+    console.log(num1);   
+    return num1+num2;
+ }
+ console.log(z(2,3))
+ console.log(z(3,5))
+
+ a = 15;
+console.log(a);
+function t(a){
+   console.log(a);   
+   return a;
+}
+b = t(10);
+console.log(b);
+
+a = 15;
+console.log(a);
+function c(a){
+   console.log(a);   
+   return a*2;
+}
+b = c(10);
+console.log(b);
